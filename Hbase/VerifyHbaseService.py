d79049f8d10c6c8f8e18658712bfc8610dafc32b
davieyang <yangdawei@youedata.com>
2018-10-25 14:34:03 +0800

    craete new

--- Hbase/VerifyHbaseService.py
+++ Hbase/VerifyHbaseService.py
@@ -10,3 +10,13 @@
 from thrift.transport import TSocket, TTransport
 from thrift.protocol import TBinaryProtocol
 from hbase import Hbase
+
+# thrift默认端口是9090
+socket = TSocket.TSocket('192.168.0.156', 9090)
+socket.setTimeout(5000)
+transport = TTransport.TBufferedTransport(socket)
+protocol = TBinaryProtocol.TBinaryProtocol(transport)
+client = Hbase.Client(protocol)
+socket.open()
+print client.getTableNames()
+print client.get('test', 'row1', 'cf:a')
